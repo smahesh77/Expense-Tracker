9588d43e0ee4f4a33571a1803eeac0c5800f3734
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2023-09-23 12:56:32 +0530

    avgwrgr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
 import './App.css'
-import Dashboard from '../Components/Dashboard'
+import Navbar from './Components/Navbar'
 function App() {
  
   return (
     <div className="App">
-      <Dashboard/>
+      <Navbar/>
     </div>
   )
 }
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import {Container,Flex, Text,Center} from '@chakra-ui/react'
-import wavehand from '../src/assets/Waving Hand.svg'
+import wavehand from '../assets/Waving Hand.svg'
 import { Select } from '@chakra-ui/react'
-import women from '../src/assets/People.svg'
+import women from '../assets/People.svg'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import {  HStack, VStack } from '@chakra-ui/react'
-const Dashboard=()=>{
+const Navbar=()=>{
   return(
-  <Flex direction='row' justify='space-between' >
-<HStack >
+  <Flex direction='row' justify='space-between'>
+<HStack align='flex-start' >
   <Text mr='4' fontSize='30' fontWeight="bold">
   Hello Jordan 
   </Text>
   <img src={wavehand} width={40}/>
-</HStack>
+  </HStack>
 <HStack>
 <Select placeholder='Personal'>
 </Select>
@@ -29,4 +29,4 @@ const Dashboard=()=>{
   )
 }
 
-export default Dashboard
+export default Navbar
