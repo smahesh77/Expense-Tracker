ec798c9117a98de567a60467bf07d59dec48dc1d
Mary Ann Jose <maryannjose129@gmail.com>
2023-09-23 15:51:29 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 
+import { Center, Flex } from '@chakra-ui/react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Sidebar from './Components/Sidebar'
 import Dashboard from './pages/Dashboard'
 function App() {
- 
+
   return (
     <div className="App">
-      <Dashboard/>
+      <Navbar />
+      <Flex>
+        <Sidebar />
+        <Dashboard />
+      </Flex>
     </div>
   )
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Sidebar from '../Components/Sidebar'
+import { Container, Text, Flex } from '@chakra-ui/react'
 function Dashboard() {
   return (
-    <div className='dashboard'>
-      <Navbar/>
-      <Sidebar/>
-    </div>
+    <Flex flexDirection={'column'} bgColor={'purple.100'} w={900}>
+      <Flex flexDirection={'column'} >
+        <Flex flexDirection={'row'}>
+          <Container w={200} h={100} flexDirection={'column'} bgColor={'purple.300'} borderRadius={10} p={5}>
+            <Text>Your income</Text>
+            <Text>$17,000</Text>
+          </Container>
+          <Container w={200} h={100} flexDirection={'column'} bgColor={'purple.300'} borderRadius={10} p={5}>
+            <Text>Your expense</Text>
+            <Text>$7,000</Text>
+          </Container>
+        </Flex>
+        <Container w={400} h={300} bgColor={'green'}>Stats</Container>
+      </Flex>
+    </Flex>
   )
 }
 
