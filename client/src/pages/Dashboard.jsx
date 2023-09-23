a843fbfd2e13409371974f4d15198556a20dca44
Mary Ann Jose <maryannjose129@gmail.com>
2023-09-23 16:00:07 +0530

    added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import { Container, Text, Flex } from '@chakra-ui/react'
 function Dashboard() {
   return (
     <Flex flexDirection={'column'} bgColor={'purple.100'} w={900}>
-      <Flex flexDirection={'column'} >
+      <Flex flexDirection={'column'} w={500} bgColor={'blue.200'} >
         <Flex flexDirection={'row'}>
           <Container w={200} h={100} flexDirection={'column'} bgColor={'purple.300'} borderRadius={10} p={5}>
             <Text>Your income</Text>
@@ -16,7 +16,7 @@ function Dashboard() {
             <Text>$7,000</Text>
           </Container>
         </Flex>
-        <Container w={400} h={300} bgColor={'green'}>Stats</Container>
+        <Container w={350} h={250} bgColor={'green'} mt={10}>Stats</Container>
       </Flex>
     </Flex>
   )
