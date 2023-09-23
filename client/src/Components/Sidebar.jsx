2930e892b7f96620c560ebd005711de415a34bfc
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2023-09-23 15:23:42 +0530

    aca

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -4,12 +4,15 @@ import { HStack, VStack } from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import dashboard from '../assets/More_Grid_Big.svg'
 import edit from '../assets/Note_Edit.svg'
+import del from '../assets/del_alt_fill.svg'
+import profile from '../assets/User_02.svg'
 const Sidebar = () => {
+    
     return (
-        <Flex  justifyContent="flex-start" >
-        <VStack>
-            <Tabs orientation="vertical">
-                <TabList>
+     
+        <VStack >
+            <Tabs  size='lg' isFitted orientation="vertical"   >
+                <TabList >
                     <Tab>
                         
                        <img src={dashboard}/>
@@ -23,14 +26,18 @@ const Sidebar = () => {
                     </Tab>
 
                     <Tab>
-          
+                      <img src={del}/>
+                      <Text>&nbsp;Bucket List</Text>
                     </Tab>
-
+          <Tab>
+            <img src={profile}/>
+            <Text>&nbsp;Profile</Text>
+          </Tab>
                 </TabList>
             </Tabs>
         </VStack>
         
-            </Flex>
+           
     )
 }
 
