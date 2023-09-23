1826198906278182d49ff011188dff33babafaf6
MaheswariDarish <maheswaridj1312@gmail.com>
2023-09-23 21:07:58 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
-
+import { Container, Flex, Heading, Image , Input , Button} from '@chakra-ui/react'
 import './App.css'
 import Navbar from './Components/Navbar'
-import Dashboard from './pages/Dashboard'
+import Profile from './pages/profile'
+import Sidebar from './Components/Sidebar'
 function App() {
  
   return (
     <div className="App">
-      <Dashboard/>
+      <Navbar/>
+      <Flex direction='row'>
+        <Flex direction={'column'}><Sidebar/></Flex>
+        <Flex direction={'column'}><Profile/></Flex>
+        </Flex>
     </div>
   )
 }
---
+++ client/src/pages/profile.jsx
@@ -0,0 +1,61 @@
+import { Container, Flex, Text, Image, Input, Button, Box, HStack } from '@chakra-ui/react'
+import React from 'react'
+import { BsPencilFill } from 'react-icons/bs';
+import { FaCamera } from "react-icons/fa";
+const Profile = () => {
+    return (
+        <Flex h={450} w={1000} borderRadius={10} mt={4} ml={5} bgColor={'purple.50'} p={4}>
+            <Flex direction={'column'}>
+                <Image ml={10}
+                    borderRadius='full'
+                    boxSize='150px'
+                    src='https://bit.ly/dan-abramov'
+                    alt='Dan Abramov'
+                />
+
+                <Flex direction={'row'} mt={14} >
+                    <Flex direction={'column'} mr={100} ml={50}>
+                        <Text as='b'>First Name:</Text>
+                        <HStack>
+                            <Text>Jordan</Text>
+                            <Button leftIcon={<BsPencilFill />} colorScheme="purple" variant="ghost"/>
+                        </HStack>
+                    </Flex>
+                    <Flex direction={'column'}>
+                        <Text as='b'>Last Name:</Text>
+                        <HStack>
+                            <Text>killian </Text>
+                            <Button leftIcon={<BsPencilFill />} colorScheme="purple" variant="ghost" />
+                        </HStack>
+                    </Flex>
+                </Flex>
+                <Flex direction={'row'} mt={11.9} mb={4}>
+                    <Flex direction={'column'} mr={125} ml={50}>
+                        <Text as='b'>Phone no:</Text>
+                        <HStack>
+                            <Text>9048576944</Text>
+            
+                        </HStack>
+                    
+                    </Flex>
+                    <Flex direction={'column'} >
+                        <Text as='b'>Email:</Text>
+                        <HStack>
+                            <Text>jordk@gmail.com</Text>
+                            
+                        </HStack>
+                    
+                    </Flex>
+                </Flex>
+                <Button ml={50}  mt={4} colorScheme="purple" w={40} borderRadius={30}>Save</Button>
+            </Flex>
+
+        </Flex>
+    );
+};
+
+export default Profile;
+
+
+
+
