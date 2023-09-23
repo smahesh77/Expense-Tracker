9a093902186f462830634920ff30499801d24b4e
Mary Ann Jose <maryannjose129@gmail.com>
2023-09-23 13:17:34 +0530

    added

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+function Dashboard() {
+  return (
+    <div className='dashboard'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Dashboard
