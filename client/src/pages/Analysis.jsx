8d58ebfb520d6586075a9663ca4305562dac374f
Mary Ann Jose <maryannjose129@gmail.com>
2023-09-25 19:16:21 +0530

    commit

--- client/src/pages/Analysis.jsx
+++ client/src/pages/Analysis.jsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Box,Heading } from "@chakra-ui/react"; // Import Box component
+import { Container, Flex, Box, Text } from "@chakra-ui/react"; // Import Box component
 import Pichartt from "../Components/Pichartt";
 import React, { useContext, useEffect, useState } from "react";
 import AuthContext from "../contexts/authContext";
@@ -23,27 +23,33 @@ const Analysis = () => {
   }, []);
 
   return (
-    <Flex h={450} w={1000} bgColor={"purple.50"} mt={4} ml={5} p={4}>
+    <Flex h={'80vh'} w={'83vw'} bgColor={"purple.50"} mt={4} ml={5} p={4} borderRadius={20} direction={'row'}  justifyContent={'space-between'}>
       <Flex direction={"row"}>
         <Container h={350} w={400}>
           <Pichartt />
         </Container>
       </Flex>
       <Box
-        rowSpan={2}
-        colSpan={1}
         bg="white"
         borderRadius={10}
-        w={350}
+        w={400}
         flexDirection={"column"}
         align={"center"}
         justifyContent={"center"}
         p={4}
+        minH={'75vh'}
         boxShadow={"9px 9px 18px #e6e6e6,-9px -9px 18px #ffffff"}
-        overflowY="auto" // Add overflow-y to make the content scrollable
+        overflowY="auto"
+        sx={
+          { 
+         '::-webkit-scrollbar':{
+                display:'none'
+            }
+         }
+       }
         maxHeight="350px" // Set a max height for the scrollable area
       >
-        <Heading marginBottom={3}>My Expenses</Heading>
+        <Text marginBottom={3} fontWeight={'bold'} fontSize={25}>My Expenses</Text>
         {items.map((i) => (
           <Itembox title={i.name} price={i.amount} category={i.cat} key={i._id} />
         ))}
